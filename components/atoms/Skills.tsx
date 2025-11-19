fd58c7da12e82d623fa9162426ed3f9892a51d48
Daniel Britto <danielbritto185@gmail.com>
2025-11-19 16:27:39 -0500

    update skill

--- components/atoms/Skills.tsx
+++ components/atoms/Skills.tsx
@@ -63,40 +63,47 @@ export default function Skills() {
                 }}></div>
 
                 <div className="skill-node absolute top-[15%] left-[20%] md:left-[28%] w-20 h-20 md:w-24 md:h-24 rounded-lg bg-gray-50 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-lg flex flex-col items-center justify-center p-1 text-center">
-                    <img alt="React Logo" className="w-6 h-6 mb-1" src="https://lh3.googleusercontent.com/aida-public/AB6AXuC1rS81c9TBM4g3ailGMtyyJZjuvXbqtEx8mccUspu4DwWq8-anjgYrByIHHjacqMU4tjjNfwF3cD-AkLkAvrlcPClHsvHRjkq6ldxYYlJoUyGjPBMXCTfU9aqJD2ECsKai3taY3SfbtNhyqJyJr8QAZr-L2FDoFaifNmHtI7e3RiKiPU97pg8HHiKs0PXZaH7Mw3XSoJ_-6y6KoBrX9b3IT1c1KMRifFJNc1r_dGvN-AY_8He3Ioup58ldBaBTx0XI0XGvtEq9DVQ"/>
+                    <svg className="w-6 h-6 mb-1">
+                        <use href="/sprite.svg#react" />
+                    </svg>
                     <h4 className="font-bold text-gray-900 dark:text-white text-xs">React</h4>
                     <p className="text-[10px] text-gray-500">Frontend</p>
                 </div>
 
 
                 <div className="skill-node absolute top-[15%] right-[20%] md:right-[28%] w-20 h-20 md:w-24 md:h-24 rounded-lg bg-gray-50 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-lg flex flex-col items-center justify-center p-1 text-center">
-                    <img alt="Next.js Logo" className="w-6 h-6 mb-1 filter dark:invert" src="https://lh3.googleusercontent.com/aida-public/AB6AXuBse452bfXnWfkbLlpTeo7qDSblBayFojp_SDy-CRtiwxoulb6I3Ne_-0KfiAbsK7lZBsyvDFW8gNZWsDAaNV928m_l64hkpHYUMqa30ENzXtNcb2xyc1mAm5H_h1f5FKaQFtLrhehTfs4lEmq45HaGL-OSLsVmLS1rk7nJFndSfI3fTRERDRTGcCSrFEadsYWx_wFU31ycaC1Y0sMhhGNVEcsWee9bZSVlyCr-LPbW0kzWRPRPt_Vd63hvlr22fjRe7pVPvaHilyE"/>
+          
+                <img className="w-7 h-7" src="/next.svg" alt="Mi logo" />
                     <h4 className="font-bold text-gray-900 dark:text-white text-xs">Next.js</h4>
                     <p className="text-[10px] text-gray-500">Full-Stack</p>
                 </div>
 
                 <div className="skill-node absolute bottom-[15%] left-[20%] md:left-[28%] w-20 h-20 md:w-24 md:h-24 rounded-lg bg-gray-50 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-lg flex flex-col items-center justify-center p-1 text-center">
-                    <img alt="Node.js Logo" className="w-6 h-6 mb-1" src="https://lh3.googleusercontent.com/aida-public/AB6AXuAf8iReeJQDzM4PBOKQxKyfGmCiVAWQMDP7BHkuB1__Uzs-bDmA3SqMNgJcKATdU4w_2gQj7QES0YM2GsJiBA74tYv6Y8vEtCMzIkE-Ejb21iNnvQsEmh70pJUmzDCyn-ETK14C7IbZJT4ytM0ZLAuhMcJJbvE4088mLPVr4TnIkHZZ8nSG77_RCwoSRiaXPwMXt1pprwHUfa1Lx_L4VbHQitLHwkSjDJVAShMv1pCv5CTRkdnK9iiJ3BczVXSYZgjHH9nvzknai9s"/>
+                    <svg className="w-6 h-6 mb-1">
+                        <use href="/sprite.svg#node" />
+                    </svg>
                     <h4 className="font-bold text-gray-900 dark:text-white text-xs">Node.js</h4>
                     <p className="text-[10px] text-gray-500">Backend</p>
                 </div>
 
 
                 <div className="skill-node absolute bottom-[15%] right-[20%] md:right-[28%] w-20 h-20 md:w-24 md:h-24 rounded-lg bg-gray-50 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-lg flex flex-col items-center justify-center p-1 text-center">
-                    <img alt="MySQL Logo" className="w-6 h-6 mb-1" src="https://lh3.googleusercontent.com/aida-public/AB6AXuCfJfqaby2UNTxe4wW7dV6T52t9-StLuptW5uReSqNX0qONrcgCFZNUlLg5IIJm8eXBe4EXdyYUNWTY-BDpMKjQtXqeIw8uzwr62mzWWEnWikivh8fjiAB3ctRegxzvmIn68S0kkE2cPTtC2fzfGicx6PlBsMInxYtiYOJfFJxGJBN2XROufXizyjfKLyUeWLe25ZKv7YFUEK3kupB5pvKgX4E5AhDgPS9m1cg44gEi7Oj-W5eB77Fc_I2vIYfqq59sBtQykGBvSi8"/>
+                <img className="w-8 h-8" src="/mysql.svg" alt="Mi logo"/>
                     <h4 className="font-bold text-gray-900 dark:text-white text-xs">MySQL</h4>
                     <p className="text-[10px] text-gray-500">Database</p>
                 </div>
 
 
                 <div className="skill-node absolute top-1/2 -translate-y-1/2 left-[5%] md:left-[20%] w-16 h-16 md:w-20 md:h-20 rounded-full bg-gray-50 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-md flex flex-col items-center justify-center p-1 text-center">
-                    <img alt="Tailwind CSS Logo" className="w-5 h-5 mb-1" src="https://lh3.googleusercontent.com/aida-public/AB6AXuBXKjcXzv5M1yeDX0sh37QbDGzoUkhNmY--5SU7S8CH4E7wc0rI8hdV1gjkkrF02hYbK-9bQJFbL1aPHeP5krZ-Dlkq6H89N2MZRoDSWGYf3Ym0KlLFsrfUJeuHbpCT1rXdLvn0aIYeQn6fnzrckLTk1reO617DUfDd2lFwLAkiLi2doFEvCPlyyGX0nOu0msIV9MwVaxRQk7uregHXd99JJYS8KqxjfoAsJ_nFpoBb0NxOo9EobBGjYhIDB7nhq1CH9ykkZUQ51fc"/>
+                    <svg className="w-5 h-5 mb-1">
+                        <use href="/sprite.svg#tailwind" />
+                    </svg>
                     <h4 className="text-[10px] font-bold text-gray-900 dark:text-white">Tailwind</h4>
                 </div>
                 
 
                 <div className="skill-node absolute top-1/2 -translate-y-1/2 right-[5%] md:right-[20%] w-16 h-16 md:w-20 md:h-20 rounded-full bg-gray-50 dark:bg-gray-800 border border-gray-300 dark:border-gray-700 shadow-md flex flex-col items-center justify-center p-1 text-center">
-                    <img alt="Docker Logo" className="w-5 h-5 mb-1" src="https://lh3.googleusercontent.com/aida-public/AB6AXuD-vpEUEuIcIq4HZTZft9tMxwInEob8R5vXUQY7slHZ8OgnfRaoWPAQbvK41PxwWjKq-x1XccYQA-RdXFLq5BWIBJKSfnozClZFT8Z3I9joNrdoMPJ9TSumV6Kxy6VZ8uHq3eAPFE3RNqrwYR2YMEiPYp39XTBPVX5GuFN08V2vt_qy5G6LxFaSn4Nc1O6a0sd5yW4Q2KSwgV0vPLGbQlDe2vilg4t6g1U4JMBc_3nbaD0j5qV19b6mf98sllnpfCVNPl3X84G2h4A"/>
+                <img className="w-7 h-7" src="/docker.svg" alt="Mi logo"/>
                     <h4 className="text-[10px] font-bold text-gray-900 dark:text-white">Docker</h4>
                 </div>
 
