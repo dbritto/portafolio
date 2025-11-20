e22199e22b00eb04c5d7ee609b7a3d7ccb31fbc9
Daniel Britto <danielbritto185@gmail.com>
2025-11-20 02:01:57 -0500

    update footer

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -5,11 +5,11 @@ export default function Footer() {
 
 
         <footer className="bg-neutral-primary-soft rounded-base shadow-xs m-4">
-            <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <div className="w-full mx-auto max-w-screen-xl  md:flex md:items-center md:justify-between">
                 <span className="text-1xl text-body sm:text-center">© 2023 <a href="https://flowbite.com/" className="hover:underline">Daniel Britto</a>.
                 </span>
                  <div className="sm:flex sm:items-center sm:justify-between">
-                <div className="mx-auto max-w-screen-xl p-4 flex justify-center">
+                <div className="mx-auto max-w-screen-xl flex justify-center">
                     <div className="flex mt-4 sm:justify-center sm:mt-0">
                         <LinkPrimary icon='github' link='https://github.com/dbritto'/>
                         <LinkPrimary icon='discord' link='#'/>
